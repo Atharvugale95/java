828b6b6e78d71a66646c4bf4cd83d5de4b0bbd9b
Atharv Ugale <158261519+goldenknights95@users.noreply.github.com>
2024-07-17 22:15:29 -0700

    switch cases in java

---
+++ Day.java
@@ -0,0 +1,41 @@
+class Day{
+public static void main(String args[])
+{
+for(int i=0;i<args.length;i++)
+{
+System.out.println(args[i]);
+
+
+switch(args[i]){
+  case "0":
+System.out.println("Sunday");
+break;
+ case "1":
+System.out.println("Monday");
+break;
+ case "2":
+System.out.println("Tuesday");
+break;
+ case "3":
+System.out.println("Wednesday");
+break;
+ case "4":
+System.out.println("thrsday");
+break;
+ case "5":
+System.out.println("Friday");
+break;
+ case "6":
+System.out.println("Saturday");
+   break;
+   
+   }
+   
+   
+   
+   }
+
+
+}
+
+}
